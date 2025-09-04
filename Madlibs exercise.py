af247e1711c4e1c3185766f5e5542404848ee523
Bhavana-ravi <148984002+Bhavana-ravi@users.noreply.github.com>
2025-09-04 12:51:59 +0530

    Create Madlibs exercise.py

---
+++ Madlibs exercise.py
@@ -0,0 +1,12 @@
+# Mad Libs Exercise
+
+noun = input("Enter a noun : ")
+adj1 = input("Enter an adjective : ")
+verb = input("Enter a verb : ")
+adj2 = input("Enter an adjective : ")
+adj3 = input("Enter an adjective : ")
+
+print(f"My name is {noun}")
+print(f"I am a very {adj1} person")
+print(f"I {verb} to work with AI & ML projects")
+print(f"I was very {adj2} person before, but now I am {adj3) person")
